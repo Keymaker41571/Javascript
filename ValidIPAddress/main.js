8d1d09172a4741d5c456fb1b7ad53d19adab9dae
Keymaker41571 <59454964+Keymaker41571@users.noreply.github.com>
2022-03-08 03:58:18 -0800

    IP Boolean check

--- ValidIPAddress/main.js
+++ ValidIPAddress/main.js
@@ -1,10 +1,14 @@
 alert("Javascript Working!!");
 
-function myFunction() {
-    let userInput = document.querySelector("#myIp");
-    let message = document.querySelector("#message");
-    
-    message.innerHTML = userInput.value;
-    
+function myIp() {
+    let emptyIp = document.getElementById("myIP").value;
+    let checkInputIp = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/     
+    if((checkInputIp.test(emptyIp))) {
+        alert("Valid ip");
+        message.innerHTML = checkInputIp.value; // no clue why it doesnt show in web browser if i take out value shows regex function but why
+                                                // regex function is created wrong and handled wrong?
+    }
+    else {
+        alert("Inva;id ip");
+    }
 }
-
